fa8935bd75300afa0eaa22eb0899fb39e3541a78
ajaybarath <ajaybarath3@gmail.com>
2021-09-03 22:16:08 +0530

    ADDED: add in a ordered list and remove if repeated

---
+++ src/com/bridgeLabs/dataStructure/OrderedList.java
@@ -0,0 +1,30 @@
+package com.bridgeLabs.dataStructure;
+
+import com.bridgeLabz.linkedList.INode;
+import com.bridgeLabz.linkedList.MyLinkedList;
+import com.bridgeLabz.linkedList.MyNode;
+
+public class OrderedList {
+
+	public static void main(String args[]) {
+
+		String sentence = "to be or not be";
+		String words[] = sentence.toLowerCase().split(" ");
+
+		MyLinkedList<String> myLinkedList = new MyLinkedList<String>();
+
+		for (String word : words) {
+
+			if (myLinkedList.findNode(new MyNode<String>(word)) == false) {
+				myLinkedList.addSortedOrder(new MyNode<String>(word));
+			} else {
+				myLinkedList.deleteNode(new MyNode<String>(word));
+			}
+
+		}
+
+		myLinkedList.printNode();
+
+	}
+
+}
