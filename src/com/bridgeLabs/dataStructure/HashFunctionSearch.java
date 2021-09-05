0eb821ade386081fca4b46a763351f377495fec1
ajaybarath <ajaybarath3@gmail.com>
2021-09-05 19:08:18 +0530

    ADDED: function to generate hash code

--- src/com/bridgeLabs/dataStructure/HashFunctionSearch.java
+++ src/com/bridgeLabs/dataStructure/HashFunctionSearch.java
@@ -1,5 +1,31 @@
 package com.bridgeLabs.dataStructure;
 
-public class HashFunctionSearch {
+import java.util.ArrayList;
 
+import com.bridgeLabz.linkedList.MyLinkedList;
+
+
+public class HashFunctionSearch<K, V> {
+
+	private final int numBuckets;
+	ArrayList<MyLinkedList> myBucketArray;
+	
+	public HashFunctionSearch() {
+		this.numBuckets = 11;
+		this.myBucketArray = new ArrayList<>(numBuckets);
+		
+		for (int i=0;i<numBuckets;i++) {
+			this.myBucketArray.add(null);
+		}
+	}
+	
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numBuckets;
+		return index;
+	}
+	
+	
+	
+	
 }
